9d0df2e60c19288c21c19965b94e04209db5697e
Sid <sidp@knights.ucf.edu>
2016-12-04 22:24:21 -0500

    Initial Commit

---
+++ index.ios.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import Root from './js/containers'
+
+AppRegistry.registerComponent('Pinz', () => Root);
---
+++ js/containers/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class Root extends Component {
+    render() {
+        return(
+            <View>
+                <Text>TEST</Text>
+            </View>
+        );
+    }
+}
