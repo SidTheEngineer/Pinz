ed41bff7d0d4658db75a32fe00d98b30fe6b6278
Sid <sidp@knights.ucf.edu>
2016-12-29 23:43:04 -0500

    Starting working on NavBar styling, using iOS navStyles

--- js/components/NavigationBar.js
+++ js/components/NavigationBar.js
@@ -9,30 +9,37 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 const styles = StyleSheet.create({
   navigationBar: {
-    backgroundColor: 'rgb(240, 240, 240)'
+    backgroundColor: 'rgb(51, 204, 94)'
   },
   iconContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  routeTitle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white'
   }
 });
 
 const NavigationBar = (
   <Navigator.NavigationBar
     style={styles.navigationBar}
+    navigationStyles={Navigator.NavigationBar.StylesIOS}
     routeMapper={{
       LeftButton: () => (
-        <TouchableHighlight style={styles.iconContainer}>
-          <Icon name="arrow-left" size={20} />
+        <TouchableHighlight style={[styles.iconContainer, { marginLeft: 20 }]}>
+          <Icon name="arrow-left" size={20} color={'white'} />
         </TouchableHighlight>
       ),
       RightButton: () => (
-        <TouchableHighlight style={styles.iconContainer}>
-          <Icon name="arrow-right" size={20} />
+        <TouchableHighlight style={[styles.iconContainer, { marginRight: 20 }]}>
+          <Icon name="arrow-right" size={20} color={'white'} />
         </TouchableHighlight>
       ),
-      Title: route => (<Text>{ route.title }</Text>)
+      Title: route => (<Text style={styles.routeTitle}>{ route.title }</Text>)
     }}
   />
 );
