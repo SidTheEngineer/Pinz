6f214cf98baa7d0f4ad62ac9535823f48b60018e
Sid <sidp@knights.ucf.edu>
2017-01-02 16:16:32 -0500

    Started adding custom marker assets

--- js/components/Map/Event.js
+++ js/components/Map/Event.js
@@ -2,7 +2,10 @@ import React from 'react';
 import MapView from 'react-native-maps';
 
 import MAP from '../../constant';
+import active from './assets/active.png';
+import place from './assets/place.png';
 
+// Supply coordinates of event based on name of location.
 const getEventCoords = (event) => {
   const landmarks = MAP.LANDMARKS.filter(
     landmark => landmark.NAME === event.location
@@ -11,10 +14,22 @@ const getEventCoords = (event) => {
   return landmarks[0].REGION;
 };
 
+// Supply a custom marker image based on category of event.
+const getEventImage = (event) => {
+  switch (event.category) {
+    case 'Sports':
+      return active;
+    default:
+      return place;
+  }
+};
+
 const Event = props => (
   <MapView.Marker
     key={props.event.event_id}
     coordinate={getEventCoords(props.event)}
+    image={getEventImage(props.event)}
+    // draggable
   />
 );
 
